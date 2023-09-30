430da5b9668ea045c7764bb5b8c98234638dae94
raquelcandida <60801737+raquelcandida@users.noreply.github.com>
2023-09-30 14:36:40 -0300

    Add files via upload
    Exercício de testes unitários parte 2 - phonebook

---
+++ eta_lab/src/phonebook.py
@@ -0,0 +1,168 @@
+import re
+
+class Phonebook:
+
+    def __init__(self):
+        self.entries = {'POLICIA': '190'}
+
+    # MELHORIA: criamos a função para validar caracteres especiais usando expressão regular
+    def valida_caracteres_especiais(self, valor):
+        regex = r"^[a-zA-Z0-9áéíóúâêîôûãõàèìòùäëïöüçñÁÉÍÓÚÂÊÎÔÛÃÕÀÈÌÒÙÄËÏÖÜÇÑ]+$"
+        return re.match(regex, valor) is None
+
+    def add(self, name, number):
+        CAMPO_VAZIO = 0
+        """
+
+        :param name: name of person in string
+        :param number: number of person in string
+        :return: 'Nome invalido' or 'Numero invalido' or 'Numero adicionado' or 'Nome ja adicionado'
+        """
+        # MELHORIA: trocamos os Ifs para usar apenas um onde valida o campo nome.
+        # nome inválido caso esteja vazio ou contenha caracteres especiais
+        # BUG: Corrigir retorno quando existiam mais de um If.
+        if len(name) == CAMPO_VAZIO or self.valida_caracteres_especiais(name):
+            return 'Nome invalido'
+
+        # BUG: na validação de vazio para campo numero
+        if len(number) == CAMPO_VAZIO:
+            return 'Numero invalido'
+
+        # MELHORIA: validação de nome duplicado
+        if name in self.entries.keys():
+            return 'Nome ja adicionado'
+
+        if name not in self.entries:
+            self.entries[name] = number
+
+        return 'Numero adicionado'
+
+    def lookup(self, name):
+        CAMPO_VAZIO = 0
+        """
+        :param name: name of person in string
+        :return: return number of person with name or 'Nome nao existe'
+        """
+        # MELHORIA: trocamos os Ifs para usar apenas um onde valida o campo nome.
+        # nome inválido caso esteja vazio ou contenha caracteres especiais
+        # BUG: Corrigir retorno quando existiam mais de um If.
+        if len(name) == CAMPO_VAZIO or self.valida_caracteres_especiais(name):
+            return 'Nome invalido'
+
+        # MELHORIA: validação de nome caso não exista no phonebook
+        if name not in self.entries.keys():
+            return 'Nome nao existe'
+
+        # BUG: corrigimos o retorno da função para retornar o nome com o número
+        return {name: self.entries[name]}
+
+    def get_names(self):
+        """
+
+        :return: return all names in phonebook
+        """
+        return self.entries.keys()
+
+    def get_numbers(self):
+        """
+
+        :return: return all numbers in phonebook
+        """
+        return self.entries.values()
+
+    def clear(self):
+        """
+        Clear all phonebook
+        :return: return 'phonebook limpado'
+        """
+        self.entries = {}
+        return 'phonebook limpado'
+
+    def search(self, search_name):
+        """
+        Search all substring with search_name
+        :param search_name: string with name for search
+        :return: return list with results of search or 'Nome nao existe'
+        """
+        result = []
+
+        for name, number in self.entries.items():
+            # BUG: corrigimos a validação do If para comparar com o name passado por parâmetro
+            # MELHORIA: equiparamos os dois valores para caixa baixa para ajudar na pesquisa
+            if search_name.lower() in name.lower():
+                # BUG: corrigimos o objeto adicionado no array.
+                result.append({name: self.entries[name]})
+        return result
+
+    def get_phonebook_sorted(self):
+        """
+
+        :return: return phonebook in sorted order
+        """
+        # BUG: corrigimos ordenando o phonebook com a função sorted
+        return sorted(self.entries)
+
+    def get_phonebook_reverse(self):
+        """
+
+        :return: return phonebook in reverse sorted order
+        """
+        # BUG: corrigimos ordenando o phonebook com a função sorted e ordenando de forma inversa com a função reversed
+        return reversed(sorted(self.entries))
+
+    def delete(self, name):
+        CAMPO_VAZIO = 0
+        """
+        Delete person with name
+        :param name: String with name
+        :return: return 'Numero deletado' or 'Nome invalido' or 'Nome nao existe'
+        """
+        # MELHORIA: validação de name vazio
+        if len(name) == CAMPO_VAZIO:
+            return 'Nome invalido'
+
+        # MELHORIA: validação de name não exista no phonebook
+        if name not in self.entries.keys():
+            return 'Nome nao existe'
+
+        self.entries.pop(name)
+        return 'Numero deletado'
+
+    # MELHORIA: adicionamos método change_number para alterar o numero de um nome
+    def change_numbers(self, name, number):
+        CAMPO_VAZIO = 0
+        """
+        Change number of person
+        :param name: String with name of person
+        :param number: String with number for change
+        :return: return 'Numero alterado' or 'Nome invalido' or 'Nome nao existe' or 'Numero invalido'
+        """
+        if len(name) == CAMPO_VAZIO:
+            return 'Nome invalido'
+
+        if name not in self.entries.keys():
+            return 'Nome nao existe'
+
+        if len(number) == CAMPO_VAZIO:
+            return 'Numero invalido'
+
+        self.entries[name] = number
+        return "Numero alterado"
+
+    # MELHORIA: adicionamos método get_name_by_number para buscar por numero
+    def get_name_by_number(self, search_number):
+        CAMPO_VAZIO = 0
+        """
+        Get name by number
+        :param search_number: String with number for search
+        :return: return list with results of search or 'Numero invalido'
+        """
+        if len(search_number) == CAMPO_VAZIO:
+            return 'Numero invalido'
+
+        result = []
+
+        for name, number in self.entries.items():
+            if search_number == number:
+                result.append(name)
+        return sorted(result)
---
+++ eta_lab/test/test_phonebook.py
@@ -0,0 +1,297 @@
+import pytest
+
+from src.phonebook import Phonebook
+
+class TestPhonebook:
+
+    def test_add_sucesso(self):
+        phonebook = Phonebook()
+        assert phonebook.add("sarita", "888888") == "Numero adicionado"
+        assert phonebook.add("renatão", "777777") == "Numero adicionado"
+        assert phonebook.add("raquel", "999999") == "Numero adicionado"
+
+    def test_add_nome_duplicado(self):
+        phonebook = Phonebook()
+        assert phonebook.add("sarita", "888888") == "Numero adicionado"
+        assert phonebook.add("sarita", "888888") == "Nome ja adicionado"
+
+    def test_add_numero_vazio(self):
+        phonebook = Phonebook()
+        assert phonebook.add("sarita", "") == "Numero invalido"
+
+    def test_add_nome_vazio(self):
+        phonebook = Phonebook()
+        assert phonebook.add("", "999999") == "Nome invalido"
+
+    @pytest.mark.parametrize("name, number, expected_result", [
+        ("renatão!", "999999", "Nome invalido"),
+        ("renatão@", "999999", "Nome invalido"),
+        ("renatão#", "999999", "Nome invalido"),
+        ("renatão$", "999999", "Nome invalido"),
+        ("renatão%", "999999", "Nome invalido"),
+        ("renatão¨", "999999", "Nome invalido"),
+        ("renatão&", "999999", "Nome invalido"),
+        ("renatão*", "999999", "Nome invalido"),
+        ("renatão(", "999999", "Nome invalido"),
+        ("renatão)", "999999", "Nome invalido"),
+        ("renatão_", "999999", "Nome invalido"),
+        ("renatão+", "999999", "Nome invalido"),
+        ("renatão{", "999999", "Nome invalido"),
+        ("renatão}", "999999", "Nome invalido"),
+        ("renatão[", "999999", "Nome invalido"),
+        ("renatão]", "999999", "Nome invalido"),
+        ("renatão|", "999999", "Nome invalido"),
+        ("renatão\\", "999999", "Nome invalido"),
+        ("renatão:", "999999", "Nome invalido"),
+        ("renatão;", "999999", "Nome invalido"),
+        ("renatão\"", "999999", "Nome invalido"),
+        ("renatão<", "999999", "Nome invalido"),
+        ("renatão>", "999999", "Nome invalido"),
+        ("renatão?", "999999", "Nome invalido"),
+        ("renatão/", "999999", "Nome invalido")
+    ])
+    def test_add_nome_caractere_especial(self, name, number, expected_result):
+        phonebook = Phonebook()
+        assert phonebook.add(name, number) == expected_result
+
+    def test_lookup_retornar_nome_e_numero(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "999999")
+
+        assert phonebook.lookup("sarita") == {"sarita": "999999"}
+
+    def test_lookup_nome_nao_existe(self):
+        phonebook = Phonebook()
+
+        assert phonebook.lookup("sarita") == "Nome nao existe"
+
+    def test_lookup_nome_vazio(self):
+        phonebook = Phonebook()
+
+        assert phonebook.lookup("") == "Nome invalido"
+
+    @pytest.mark.parametrize("name, expected_result", [
+        ("renatão!", "Nome invalido"),
+        ("renatão@", "Nome invalido"),
+        ("renatão#", "Nome invalido"),
+        ("renatão$", "Nome invalido"),
+        ("renatão%", "Nome invalido"),
+        ("renatão¨", "Nome invalido"),
+        ("renatão&", "Nome invalido"),
+        ("renatão*", "Nome invalido"),
+        ("renatão(", "Nome invalido"),
+        ("renatão)", "Nome invalido"),
+        ("renatão_", "Nome invalido"),
+        ("renatão+", "Nome invalido"),
+        ("renatão{", "Nome invalido"),
+        ("renatão}", "Nome invalido"),
+        ("renatão[", "Nome invalido"),
+        ("renatão]", "Nome invalido"),
+        ("renatão|", "Nome invalido"),
+        ("renatão\\", "Nome invalido"),
+        ("renatão:", "Nome invalido"),
+        ("renatão;", "Nome invalido"),
+        ("renatão\"", "Nome invalido"),
+        ("renatão<", "Nome invalido"),
+        ("renatão>", "Nome invalido"),
+        ("renatão?", "Nome invalido"),
+        ("renatão/", "Nome invalido")
+    ])
+    def test_lookup_nome_caractere_especial(self, name, expected_result):
+        phonebook = Phonebook()
+        assert phonebook.lookup(name) == expected_result
+
+    def test_get_names(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        assert 'sarita' in phonebook.get_names()
+        assert 'renatão' in phonebook.get_names()
+        assert 'raquel' in phonebook.get_names()
+
+    def test_get_numbers(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        assert '888888' in phonebook.get_numbers()
+        assert '777777' in phonebook.get_numbers()
+        assert '999999' in phonebook.get_numbers()
+
+    def test_phonebook_esta_vazio(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        phonebook.clear()
+        assert phonebook.clear() == "phonebook limpado"
+        assert len(phonebook.entries) == 0
+
+    def test_search(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        assert phonebook.search("sarita") == [{"sarita": "888888"}]
+
+    def test_search_nome_nao_existe(self):
+        phonebook = Phonebook()
+
+        assert phonebook.search("sarita") == []
+
+    def test_search_substrings_nome(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        expected_result = \
+            [{'POLICIA': '190'},
+             {'sarita': '888888'},
+             {'renatão': '777777'},
+             {'raquel': '999999'}]
+
+        assert phonebook.search("a") == expected_result
+
+    def test_get_phonebook_sorted(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        phonebook_sorted = phonebook.get_phonebook_sorted()
+
+        assert list(phonebook_sorted) == ["POLICIA", "raquel", "renatão", "sarita"]
+
+    def test_get_phonebook_reverse(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        phonebook_reverse_sorted = phonebook.get_phonebook_reverse()
+
+        assert list(phonebook_reverse_sorted) == ["sarita", "renatão", "raquel", "POLICIA"]
+
+    def test_delete(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        assert phonebook.delete("renatão") == "Numero deletado"
+        assert "renatão" not in phonebook.entries
+
+    def test_delete_nome_nao_existe(self):
+        phonebook = Phonebook()
+
+        assert phonebook.delete("renatão") == "Nome nao existe"
+
+    def test_delete_nome_vazio(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        assert phonebook.delete("") == "Nome invalido"
+
+    @pytest.mark.parametrize("caractere, expected_result", [
+        ("!", None),
+        ("@", None),
+        ("#", None),
+        ("$", None),
+        ("%", None),
+        ("&", None),
+        ("*", None),
+        ("(", None),
+        (")", None),
+        ("_", None),
+        ("+", None),
+        ("-", None),
+        ("=", None),
+        ("{", None),
+        ("}", None),
+        ("[", None),
+        ("]", None),
+        ("|", None),
+        ("\\", None),
+        (":", None),
+        (";", None),
+        ("\"", None),
+        ("'", None),
+        ("<", None),
+        (">", None),
+        ("?", None),
+        ("/", None),
+        ("^", None),
+        ("~", None),
+        ("`", None)
+    ])
+    def test_valida_caracteres_especiais(self, caractere, expected_result):
+        phonebook = Phonebook()
+        assert phonebook.valida_caracteres_especiais(caractere)
+
+    def test_change_numbers(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        expected_result = phonebook.change_numbers("raquel", "44444444")
+
+        assert expected_result == "Numero alterado"
+        assert phonebook.search("raquel") == [{"raquel": "44444444"}]
+
+    def test_change_numbers_name_invalido(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        expected_result = phonebook.change_numbers("", "44444444")
+
+        assert expected_result == "Nome invalido"
+
+    def test_change_numbers_name_nao_existe(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+
+        expected_result = phonebook.change_numbers("raquel", "44444444")
+
+        assert expected_result == "Nome nao existe"
+
+    def test_change_numbers_numero_vazio(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "777777")
+        phonebook.add("raquel", "999999")
+
+        expected_result = phonebook.change_numbers("raquel", "")
+
+        assert expected_result == "Numero invalido"
+
+    def test_get_name_by_number(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "888888")
+        phonebook.add("raquel", "999999")
+
+        expected_result = phonebook.get_name_by_number("888888")
+
+        assert expected_result == ["renatão", "sarita"]
+
+    def test_get_name_by_number_vazio(self):
+        phonebook = Phonebook()
+        phonebook.add("sarita", "888888")
+        phonebook.add("renatão", "888888")
+        phonebook.add("raquel", "999999")
+
+        expected_result = phonebook.get_name_by_number("")
+
+        assert expected_result == "Numero invalido"
